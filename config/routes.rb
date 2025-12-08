c022ac1b29ef88baf0afec282716a61d75e0ff7c
leonardomag31-lab <leonardomag31@gmail.com>
2025-12-08 15:56:14 -0300

    Home and about added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htm
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
