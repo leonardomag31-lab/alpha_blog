7ac52db904738b1d9b6930b0f05e54e845f4d997
leonardomag31-lab <leonardomag31@gmail.com>
2025-12-10 16:43:15 -0300

    dry e alterações.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htm
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
   end
 
 
