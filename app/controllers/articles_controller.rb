4e78ef83ee71554c38fea88ca0e365a1657bd303
leonardomag31-lab <leonardomag31@gmail.com>
2025-12-10 14:53:37 -0300

    Updates with edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,16 +12,31 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
-    redirect_to articles_path
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
     else
-    render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
+    end
   end
-end
 
-private
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+private #comentar
 
   def article_params
     params.require(:article).permit(:title, :description)
