d837dc940a93eb65dc30ac35eb943cc9a4d6de0f
leonardomag31-lab <leonardomag31@gmail.com>
2025-12-11 16:28:59 -0300

    Fix: Add missing core Stimulus dependencies and verify imports

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import { Application } from '@hotwired/stimulus'
+import { application } from '@hotwired/stimulus'
 eagerLoadControllersFrom("controllers", application)
