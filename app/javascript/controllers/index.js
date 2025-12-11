a0fba44dd859d803937b21572d47a56e20cecd93
leonardomag31-lab <leonardomag31@gmail.com>
2025-12-11 16:00:05 -0300

    Fix: Add stimulus-loading and correct relative path in index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
+import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import { Applvication } from 'stimulus'
-import { application } from './application'
+import { Application } from 'stimulus'
 eagerLoadControllersFrom("controllers", application)
