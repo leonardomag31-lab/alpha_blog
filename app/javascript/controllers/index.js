35306ef5eab48965f3e14dc51453a98326c373d5
leonardomag31-lab <leonardomag31@gmail.com>
2025-12-11 16:47:34 -0300

    Fix Stimulus structure for Rails 8

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,8 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-loading"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 const application = Application.start()
-const context = require.context("controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
 
-export { application }
+const context = require.context(".", true, /\.js$/)
+application.load(definitionsFromContext(context))
