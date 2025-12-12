6ca830994227e14dc6ea2692e3abb8621159a4e5
leonardomag31-lab <leonardomag31@gmail.com>
2025-12-12 14:49:19 -0300

    Fix Node engine and Bootstrap dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,43 @@
-const path    = require("path");
-const webpack = require("webpack");
+const path = require("path")
+const webpack = require("webpack")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
-
   entry: {
     application: "./app/javascript/application.js"
   },
-
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
-
-  resolve: {
-    extensions: [".js"]
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
+    ]
   },
-
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 })
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
   ]
-};
+}
